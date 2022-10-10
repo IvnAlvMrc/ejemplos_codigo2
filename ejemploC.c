a7350a46c54728a29d0dff03dadbeac2f6e38eb5
a17ivanam <ivanalvesvigo@gmail.com>
2022-10-10 16:59:24 +0200

    Añadido ejemploC. Probando el gitignore

---
+++ ejemploC.c
@@ -0,0 +1,16 @@
+/*
+comentario de varias líneas
+*/
+
+// comentarios
+
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+	
+	char cadena[] = "Introduzca primer número: ";
+	
+	printf(cadena);
+
+}
